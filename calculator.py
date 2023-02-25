7a7d99ca414847df8ce4a1f85d5f118d6c0b9ddd
Tiela Black-Law <tiela.rose@gmail.com>
2023-02-25 13:36:43 -0800

    add user input, quit function

--- calculator.py
+++ calculator.py
@@ -4,4 +4,27 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
                         power, mod, )
 
 
-# Replace this with your code
+# while exit_condition_not_reached:
+#     input = consume_input()
+#     output = evaluate_input(input)
+#     print(output)
+
+while True:
+    # get user input in the form [operation] [num1] [num2]
+    input_string = input('What do you want to calculate? ')
+    # sample input would be '+ 1 2'
+
+    # separate the input string into usable parts
+    input_tokens = input_string.split(' ')
+    operation = input_tokens[0]
+
+    if 'q' in operation:
+        print('You will exit')
+        break
+    else:
+        num1 = input_tokens[1]
+        num2 = input_tokens[2]
+
+        output = f'num1 is {num1} and num2 is {num2}'
+
+    print(output)
